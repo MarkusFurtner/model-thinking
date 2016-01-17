5be0c637167d4839a3a7203197a0667599816786
Mark Probst <mark.probst@gmail.com>
2016-01-17 08:47:31 -0800

    Actually use floating point arithmetic.

--- China.py
+++ China.py
@@ -2,7 +2,7 @@
 
 class Model:
     def __init__(self):
-        self.M = 900 #number of machines or capital
+        self.M = 900.0 #number of machines or capital
         self.year = 0
         #The value on position n represents the number of people of age n in society 
         self.demo = [
@@ -28,14 +28,14 @@ class Model:
         return self.population() - self.workers()
 
     def age_demography(self):
-        self.demo = [int(self.workers() / 50)] + self.demo[0:99]
+        self.demo = [self.workers() / 50.0] + self.demo[0:99]
         
     def step(self):
         self.age_demography()
         L = self.workers()
         N = self.nonworkers()
         
-        adv = self.year / 30
+        adv = self.year / 30.0
         
         O = ((1+adv)*L*self.M)**0.5 #workers get more qualified 
         I = O-0.5*N-L*(0.5+adv) #workers consume increases
@@ -54,8 +54,8 @@ print model.workers(), "workers"
 print model.nonworkers(), "nonworkers"
 
 #every year the demography shifts and the newborn dependent on workers
-print ("growth rate, output, Capital, Investment, Ration Workers/Nonworkers, citzens")
+print ("Growth rate, Output, Capital, Investment, Ratio workers/nonworkers, Population")
 for i in range(0, 50):
     (R, O, M, I, L, N) = model.step()
     print int(R*1000), int(O), int(M), int(I) , int((L/N)*100), int(model.population())
-print model.demo
+print [int(x) for x in model.demo]
